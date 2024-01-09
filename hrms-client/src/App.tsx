921bbc2683a81ec76a3c5b7f1fa11481dffbd55a
okruti-rkishor <rkishor@okruti.com>
2024-01-09 18:09:01 +0530

    typescript test

--- hrms-client/src/App.tsx
+++ hrms-client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React now
+          Learn React now first
         </a>
       </header>
     </div>
