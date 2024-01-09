b2dab5c23cb7f2369eb2cf4308d3d61aa91535ff
megauravjoshi <gjoshi@okruti.com>
2024-01-09 17:42:18 +0530

    test commit

--- hrms-client/src/App.tsx
+++ hrms-client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.orgs"
           target="_blank"
           rel="noopener noreferrer"
         >
