5ffe97c017b5069c30818c196f94acff3101bc81
megauravjoshi <gjoshi@okruti.com>
2024-01-10 11:56:06 +0530

    created side navbar

--- hrms-client/src/App.tsx
+++ hrms-client/src/App.tsx
@@ -1,35 +1,12 @@
 import './App.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Link,
-} from "react-router-dom";
-import Login from './component/login/login';
+import Navbar from "./component/side navbar/navbar";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <h1>Hello World</h1>
-        <Link to="/login">Login Us</Link>
-      </div>
-    ),
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <h1>Signup</h1>,
-  },
-]);
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <Navbar />
+
     </div>
   );
 }
---
+++ hrms-client/src/component/side navbar/navbar.tsx
@@ -0,0 +1,103 @@
+import React, {useState} from 'react';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import {createBrowserRouter, Link, RouterProvider} from "react-router-dom";
+import Login from "../login/login";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    // getItem('Option 1', '1', <PieChartOutlined />),
+    // getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('User 1', '3'),
+        getItem('User 2', '4'),
+        getItem('User 3', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    // getItem('Files', '9', <FileOutlined />),
+];
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <div>
+                <h1>Hello World</h1>
+                <Link to="/login">Login Us</Link>
+            </div>
+        ),
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/signup",
+        element: <h1>Signup</h1>,
+    }
+]);
+
+const Navbar: React.FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Header style={{ padding: 0, background: colorBgContainer }} />
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <div className="demo-logo-vertical" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            </Sider>
+            <Layout>
+                <Content>
+                    {/*<Breadcrumb style={{ margin: '16px 0' }}>*/}
+                    {/*    <Breadcrumb.Item>User</Breadcrumb.Item>*/}
+                    {/*    <Breadcrumb.Item>Bill</Breadcrumb.Item>*/}
+                    {/*</Breadcrumb>*/}
+                    {/*<div*/}
+                    {/*    style={{*/}
+                    {/*        padding: 24,*/}
+                    {/*        minHeight: 360,*/}
+                    {/*        background: colorBgContainer,*/}
+                    {/*        borderRadius: borderRadiusLG,*/}
+                    {/*    }}*/}
+                    {/*>*/}
+                    {/*    Bill is a cat.*/}
+                    {/*</div>*/}
+                    <RouterProvider router={router} />
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default Navbar;
