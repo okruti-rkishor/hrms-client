729a2e26a5d102819744c2d36e453a3bdc3d6bb1
okruti-rkishor <rkishor@okruti.com>
2024-02-01 16:36:34 +0530

    create context about user login and birthday data

---
+++ hrms-client/src/context/userLoginContext.tsx
@@ -0,0 +1,48 @@
+import React, {
+  createContext,
+  ReactNode,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+export interface UserLoginInterface {
+  loginStatus: boolean;
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+}
+
+export interface UserContextType {
+  newUser?: UserLoginInterface | null;
+  setNewUser?: Dispatch<SetStateAction<UserLoginInterface | null>>;
+}
+
+const UserLoginContext = createContext<UserContextType>({});
+
+export const UserLoginContextProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [newUser, setNewUser] = useState<UserLoginInterface | null>({
+    loginStatus: false,
+    id: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+    role: "",
+  });
+  const contextValue: UserContextType = {
+    newUser,
+    setNewUser,
+  };
+
+  return (
+    <UserLoginContext.Provider value={contextValue}>
+      {children}
+    </UserLoginContext.Provider>
+  );
+};
+
+export default UserLoginContext;
